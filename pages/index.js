51725f8758a2002a09d3731ee20847528219abe2
sarasotafingerprints <mike@sarasotafingerprints.com>
2023-05-25 10:33:44 -0400

    Update index.js
    added transitiveSearchParams

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,6 @@ import { Widget } from '@typeform/embed-react'
 
 export default function Home() {
   return (
-    <Widget id="StIuBpvq" style={{ width: '100%', height: '100vh' }} className="my-form" inlineOnMobile={true} hidden={{ kiosk: '0'}}/>
+    <Widget id="StIuBpvq" style={{ width: '100%', height: '100vh' }} className="my-form" inlineOnMobile={true} hidden={{ kiosk: '0'}} transitiveSearchParams={["ori", "service_type", "promo_code", "appointment_type", "oca", "contract"]}/>
   )
 }
