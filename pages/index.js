c8a0b32aa4a82515dd8ae021fa0ff9e788239a30
sarasotafingerprints <mike@sarasotafingerprints.com>
2023-05-29 13:17:06 -0400

    Update index.js
    added a bunch of params

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,6 @@ import { Widget } from '@typeform/embed-react'
 
 export default function Home() {
   return (
-    <Widget id="StIuBpvq" style={{ width: '100%', height: '100vh' }} className="my-form" inlineOnMobile={true} hidden={{ contract_owner: '1'}} transitiveSearchParams={["ori", "service_type", "promo_code", "appointment_type", "oca", "contract"]}/>
+    <Widget id="StIuBpvq" style={{ width: '100%', height: '100vh' }} className="my-form" inlineOnMobile={true} hidden={{ contract_owner: '1'}} transitiveSearchParams={["ori", "service_type", "promo_code", "appointment_type", "oca", "contract", "first_name", "last_name", "email", "initial", "demographics_needed"]}/>
   )
 }
